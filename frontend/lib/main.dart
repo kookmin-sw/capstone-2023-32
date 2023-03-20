2d1b121fdd750513c455387bc907831016a44592
cheolung12 <109952479+cheolung12@users.noreply.github.com>
2023-03-20 17:44:59 +0900

    refactor [FE] : add pages directory

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,6 +1,7 @@
+import 'package:fasttrip/pages/Home.dart';
 import 'package:flutter/material.dart';
 import './style.dart' as theme;
-import 'package:carousel_slider/carousel_slider.dart';
+
 
 void main() {
   runApp(
@@ -34,6 +35,8 @@ class MyApp extends StatelessWidget {
   }
 }
 
+
+// 하단바 커스텀
 class BottomBar extends StatelessWidget {
   const BottomBar({Key? key}) : super(key: key);
 
@@ -85,145 +88,4 @@ class BottomBar extends StatelessWidget {
     );
   }
 }
-
-class Home extends StatelessWidget {
-  const Home({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return ListView(
-      children: [
-        MainPage(),
-        Slider(),
-      ],
-    );
-  }
-}
-
-class MainPage extends StatelessWidget {
-  const MainPage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.only(bottom: 50),
-      height: MediaQuery.of(context).size.height * 0.5, // 화면 크기의 절반
-      decoration: BoxDecoration(
-        image: DecorationImage(
-          image: AssetImage('assets/flutter.png'), // 배경 이미지
-          fit:BoxFit.cover,
-        )
-      ),
-      child: Column(
-        children: [
-          Container(
-            padding: EdgeInsets.all(20),
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Expanded (
-                  child: Image.asset(   // 로고이미지
-                    'assets/flutter.png',
-                    width: 200,
-                    height: 100,
-                    alignment: Alignment.topLeft,
-                  ),
-                ),
-                IconButton(
-                  onPressed: (){},
-                  icon: Icon(Icons.search),
-                  alignment: Alignment.topRight
-                ),
-                IconButton(
-                    onPressed: (){},
-                    icon: Icon(Icons.notifications),
-                    alignment: Alignment.topRight
-                ),
-              ],
-            ),
-          ),
-          Container(
-            height: 180,    // 반응형 디자인 적용 필요
-            alignment: Alignment.bottomCenter,
-            child: ElevatedButton(
-                onPressed: (){},
-                child: Text('새로운 Plan 시작하기')
-            ),
-          ),
-        ],
-      ),
-
-    );
-  }
-}
-
-class Slider extends StatefulWidget {
-  const Slider({Key? key}) : super(key: key);
-
-
-  @override
-  State<Slider> createState() => _SliderState();
-}
-
-var heading1 = TextStyle(
-  fontWeight: FontWeight.bold,
-  fontSize: 20,
-);
-
-class _SliderState extends State<Slider> {
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.only(bottom: 50),
-      child: Column(
-        children: [
-          Container(
-            padding: EdgeInsets.only(left: 10),
-            margin: EdgeInsets.only(bottom: 20),
-            alignment: Alignment.topLeft,
-            child: Text(
-                '오늘의 Place',
-              textAlign: TextAlign.left,
-              style: heading1,
-            ),
-          ),
-          CarouselSlider(
-            options: CarouselOptions(
-              height: 200.0,
-              autoPlay: true,
-            ),
-            items: [1,2,3,4,5].map((i) {
-              return Builder(
-                builder: (BuildContext context) {
-                  return Container(
-                      width: MediaQuery.of(context).size.width,
-                      margin: EdgeInsets.symmetric(horizontal: 5.0),
-                      decoration: BoxDecoration(
-                          border: Border.all(
-                            width: 3,
-                            color: Colors.grey,
-                          ),
-                        borderRadius: BorderRadius.circular(16.0),
-                      ),
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(16.0),
-                        child: Image.asset(
-                          'assets/flutter.png',
-                          fit: BoxFit.fill,
-                        )
-                      )
-                  );
-                },
-              );
-            }).toList(),
-          ),
-          Text('설명글'),
-        ],
-      ),
-    );
-  }
-}
-
-
 
