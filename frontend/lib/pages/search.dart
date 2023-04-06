0d43397b17c1e5ed2c8778d9ced4c06cf37dbaee
cheolung12 <109952479+cheolung12@users.noreply.github.com>
2023-04-06 20:21:27 +0900

    Rename search -> Search

--- frontend/lib/pages/search.dart
+++ frontend/lib/pages/search.dart
@@ -1,6 +1,6 @@
 import 'package:fasttrip/style.dart';
 import 'package:flutter/material.dart';
-import 'package:fasttrip/pages/searchResult.dart';
+import 'package:fasttrip/pages/SearchResult.dart';
 
 class SearchPage extends StatelessWidget {
   const SearchPage({Key? key}) : super(key: key);
