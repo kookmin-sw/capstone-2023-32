0a936c30de67757156762810af1a46d35a91d5a4
cheolung12 <109952479+cheolung12@users.noreply.github.com>
2023-04-09 20:54:58 +0900

    add signature color

--- frontend/lib/style.dart
+++ frontend/lib/style.dart
@@ -7,3 +7,5 @@ var heading2 = TextStyle(fontSize: 14, fontWeight: FontWeight.bold);
 
 // 연한 글씨 스타일
 var lightText = TextStyle(color: Colors.grey.shade500);
+
+var signatureColor = Color(0xff9CC4FF);
