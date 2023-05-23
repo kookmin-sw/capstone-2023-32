01e1727eeb150354d462bae4c08c072a26e468b0
cheolung12 <109952479+cheolung12@users.noreply.github.com>
2023-05-23 21:23:33 +0900

    Update PostData.dart

--- frontend/lib/Data/PostData.dart
+++ frontend/lib/Data/PostData.dart
@@ -20,7 +20,7 @@ final List<Post> data = [
     imageUrl:
         'https://images.unsplash.com/photo-1583265266785-aab9e443ee68?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80',
     title: '사랑과 낭만의 도시 Paris',
-    tags: ['계획', '프랑스', '유럽여행', '혼자'],
+    tags: ['프랑스', '혼자', '아이스크림', '러시아'],
     heart: true,
     contents: "aaa",
   ),
@@ -28,15 +28,15 @@ final List<Post> data = [
     imageUrl: 'https://images.unsplash.com/photo-1553195029-754fbd369560?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1476&q=80',
     title: '휴양지하면 보라카이',
     tags: ['계획', '보라카이', '휴가', '가족'],
-    heart: true,
+    heart: false,
     contents: "bbb",
   ),
   Post(
     imageUrl:
         'https://images.unsplash.com/photo-1590253230532-a67f6bc61c9e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1431&q=80',
-    title: 'Chainsawman 님의 여행 계획',
-    tags: ['모집', '일본', '오사카', '단체(혼성)'],
-    heart: false,
+    title: '오사카 여행 모집합니다~!',
+    tags: ['오사카', '일본', '단체', '모집'],
+    heart: true,
     contents: "ccc",
   ),
 
