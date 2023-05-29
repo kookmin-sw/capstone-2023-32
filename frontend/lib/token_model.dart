479e539fa7e0afe7e29dbccfa5e74f7762b53b7c
cheolung12 <109952479+cheolung12@users.noreply.github.com>
2023-05-29 22:07:57 +0900

    edit token storage

--- frontend/lib/token_model.dart
+++ frontend/lib/token_model.dart
@@ -1,12 +1,29 @@
 import 'package:flutter/foundation.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class TokenModel extends ChangeNotifier {
   String? _token;
 
   String? get token => _token;
 
-  set token(String? value) {
+  Future<void> setToken(String? value) async {
     _token = value;
+    await _saveToken(value);
     notifyListeners();
   }
+
+  Future<void> _saveToken(String? token) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setString('jwt', token ?? '');
+  }
+
+  Future<void> loadToken() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String? token = prefs.getString('jwt');
+    if (token != null && token.isNotEmpty) {
+      _token = token;
+      notifyListeners();
+    }
+  }
 }
+
